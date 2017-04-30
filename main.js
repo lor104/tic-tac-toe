65e9314d5316859d6f1499f81456e3bf55fb0e54
lor104 <lauren.rosentzveig@gmail.com>
2017-04-29 21:45:29 -0400

    adjust function to display X or O based on click and player turn

--- main.js
+++ main.js
@@ -1,8 +1,19 @@
 $(document).ready(function() {
+  var count = 1;
 
   $('div.ttt-box').on('click', function() {
-    $(this).css('background-color', 'red');
-    $(this).text('X')
+  if (count % 2 === 0) {
+    $(this).text("O");
+  } else {
+    $(this).text("X");
+  }
+    count++
   });
 
 });
+
+// count = 1..9
+// if count % 2 === 0 then its an even number, and the second player (o)
+// else its the first player (x)
+//
+// when element with specific id gets clicked, depending on the count variable, adjust the colour of the background (red for x, blue for o)
